e2c2c8faed72544f11781f6f44e839e425f58c0a
Ben Doan <ben@bendoan.me>
2018-02-28 13:52:16 -0600

    added weather, menu, calendar data

--- daily-brief.py
+++ daily-brief.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python
 
 import argparse
+import datetime
 import json
 import os
-import urllib
+
+import httplib2
+import requests
+
+from apiclient import discovery
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.file import Storage
 
 from lib.bottle import (
     default_app,
@@ -17,12 +25,27 @@ from lib.bottle import (
     TEMPLATE_PATH,
 )
 
+from get_menu_img import get_menu_img
+
 WEATHER_URL = "https://api.weather.gov/points/41.252363,-95.997988/forecast"
+
+SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
+CLIENT_SECRET_FILE = 'private/client_id.json'
+APPLICATION_NAME = 'Google Calendar API Python Quickstart'
 
 
 @get('/')
 def index():
-    return template("index.tpl", title="index")
+    events = get_events()
+    weather_data = get_weather_data()
+    menu_img_url = get_menu_url()
+    return template(
+        "index.tpl",
+        title="index",
+        weather_data=weather_data,
+        events=events,
+        menu_img_url=menu_img_url
+    )
 
 
 @route('/static/<path:path>')
@@ -41,8 +64,60 @@ def strip_path():
     request.environ['PATH_INFO'] = request.environ['PATH_INFO'].rstrip('/')
 
 
-def main():
-    pass
+def get_weather_data():
+    return requests.get(WEATHER_URL).json()
+
+
+def get_credentials():
+    """Gets valid user credentials from storage.
+
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
+
+    Returns:
+        Credentials, the obtained credential.
+    """
+    home_dir = os.path.expanduser('~')
+    credential_dir = os.path.join(home_dir, '.credentials')
+    if not os.path.exists(credential_dir):
+        os.makedirs(credential_dir)
+    credential_path = os.path.join(credential_dir,
+                                   'calendar-python-quickstart.json')
+
+    store = Storage(credential_path)
+    credentials = store.get()
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = "daily-brief"
+        credentials = tools.run_flow(flow, store, None)
+        print('Storing credentials to ' + credential_path)
+    return credentials
+
+
+def get_menu_url():
+    today = datetime.datetime.strftime(datetime.datetime.now(), "%Y-%m-%d")
+    return get_menu_img(today, "Lunch")
+
+
+def get_events():
+    credentials = get_credentials()
+    http = credentials.authorize(httplib2.Http())
+    service = discovery.build('calendar', 'v3', http=http)
+
+    now = datetime.datetime.utcnow().isoformat() + 'Z'  # 'Z' indicates UTC time
+    print('Getting the upcoming 10 events')
+    eventsResult = service.events().list(
+        calendarId='primary', timeMin=now, maxResults=10, singleEvents=True,
+        orderBy='startTime').execute()
+    events = eventsResult.get('items', [])
+
+    return events
+
+    # if not events:
+    #     print('No upcoming events found.')
+    # for event in events:
+    #     start = event['start'].get('dateTime', event['start'].get('date'))
+    #     print(start, event['summary'])
 
 
 tpl_path = os.path.join(get_script_rel_path("templates"))
@@ -61,7 +136,7 @@ if __name__ == '__main__':
     with open(args.config) as f:
         config = json.load(f)
 
-    run(host='0.0.0.0', port=config['port'])
+    run(host='0.0.0.0', port=config['port'], reloader=True)
 
 app = default_app()
 
--- templates/index.tpl
+++ templates/index.tpl
@@ -1,2 +1,42 @@
 % rebase('base.tpl', title=title)
 <h2>Brief</h2>
+<div class="row">
+    % for period in weather_data['properties']['periods']:
+    <span class="weather-day col-md-2">
+        <div class="inner">
+            <div><img src="{{ period['icon'] }}" /></div>
+            <div><strong>{{ period['name'] }}</strong></div>
+            <div>{{ period['temperature'] }}&#8457;</div>
+            <div>{{ period['shortForecast'] }}</div>
+        </div>
+    </span>
+    % end
+</div>
+
+<div class="row">
+    <img src="{{ menu_img_url }}" />
+</div>
+
+<div class="row">
+    <ul>
+        % for event in events:
+            <li>
+                {{ event['summary'] }}: {{ event['start'].get('dateTime', event['start'].get('date')) }}
+            </li>
+        % end
+    </ul>
+</div>
+
+<style>
+.weather-day {
+    background-color: #eee;
+    margin: 5px;
+    height: 200px;
+}
+
+.weather-day .inner img {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+}
+</style>
